b96387546d11dc29b9375aa91b3ad628ef964beb
logbeats <shyou@logbeats.co.kr>
2021-05-27 18:54:34 +0900

    dprocess update

--- README.md
+++ README.md
@@ -36,9 +36,15 @@ pip3 install -r requirements.txt
            or
     python3 dCairos.py
    ```
-3. 실행이 완료되면 데이터 마이닝 / 분석 / 분류 등을 자동으로 수행하여 결과를 얻습니다.(Upon completion of execution, data mining/analysis/classification, etc. are automatically performed to obtain results)
+3. 아래와 같이`-d`의 인수 값을 입력하면 지정된 프로세스가 실행됩니다.(Entering the argument value of `-d` as shown below executes the specified process.)
+   ```
+    python dCairos.py -d
+           or
+    python3 dCairos.py -d
+   ```
+4. 실행이 완료되면 데이터 마이닝 / 분석 / 분류 등을 자동으로 수행하여 결과를 얻습니다.(Upon completion of execution, data mining/analysis/classification, etc. are automatically performed to obtain results)
    ![dCairosQuest](https://user-images.githubusercontent.com/46318494/119313293-57e6b180-bcae-11eb-8199-86c8070e6fc0.jpg)
-4. 아래 표는 기본 디렉토리 구조를 보여줍니다.(The table below shows the main directory structure)
+5. 아래 표는 기본 디렉토리 구조를 보여줍니다.(The table below shows the main directory structure)
    Datawork|extraction|Classification|
    ----|----|----|
    workspace|script|conf|
--- dCairosQuest.py
+++ dCairosQuest.py
@@ -6,21 +6,24 @@ from dCairosFilelib import DataUnpack
 from dCairosFilelib import FindDataFiles
 from dCairosFilelib import Initialize
 from dCairoslib import dCairos
+import argparse
 from tqdm import tqdm
 
-
+parser = argparse.ArgumentParser(description='dCairosQuest argument')
+parser.add_argument("-d", "--dprocess", dest="dprocess", action="store_true")
 # __init__#####################################################################
 basedir = os.path.abspath(os.path.dirname(__file__))
 init = Initialize(basedir)
 init.dirCheck()
 location = init.run()
-for name in tqdm(os.listdir(location['input']), desc='데이터마이닝[DataMining]'):
-    DataToProcess(location).DataMining(name)
+args = parser.parse_args()
+
+if args.dprocess:
+    for name in tqdm(os.listdir(location['input']), desc='데이터마이닝[DataMining]'):
+        DataToProcess(location).DataMining(name)
 ###############################################################################
-for dataname in tqdm(FindDataFiles(location['project'], 'zip').keys(),
-                     desc='데이터분석[DataAnalysis]'):
-    Criterion = DataAnalysis(location['conf']).BaseLine(dataname)
-    Object = DataUnpack(location['project']).UnpackExtract(dataname)
-    Proto = DataAnalysis(location['conf']).BaseReport(dataname)
-    dCairos(Criterion, Object, Proto).dWorkerRun(location['output']+'/'
-                                                 + dataname)
+    for dataname in tqdm(FindDataFiles(location['project'], 'zip').keys(), desc='데이터분석[DataAnalysis]'):
+        Criterion = DataAnalysis(location['conf']).BaseLine(dataname)
+        Object = DataUnpack(location['project']).UnpackExtract(dataname)
+        Proto = DataAnalysis(location['conf']).BaseReport(dataname)
+        dCairos(Criterion, Object, Proto).dWorkerRun(location['output']+'/'+ dataname)
