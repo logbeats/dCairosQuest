588d3906fd54fbd9725622faf8c55e7c5bac3f09
logbeats <shyou@logbeats.co.kr>
2021-06-01 12:10:39 +0900

    update dCairosQuest

--- dCairosQuest.py
+++ dCairosQuest.py
@@ -10,20 +10,32 @@ import argparse
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='dCairosQuest argument')
-parser.add_argument("-d", "--dprocess", dest="dprocess", action="store_true")
+parser.add_argument("-m", "--dMining", dest="dMining", action="store_true")
+parser.add_argument("-a", "--dAnalysis", dest="dAnalysis", action="store_true")
+parser.add_argument("-c", "--aControl", dest="aControl", action="store_true")
 # __init__#####################################################################
 basedir = os.path.abspath(os.path.dirname(__file__))
 init = Initialize(basedir)
 init.dirCheck()
 location = init.run()
 args = parser.parse_args()
-
-if args.dprocess:
+###############################################################################
+if args.dMining:
     for name in tqdm(os.listdir(location['input']), desc='데이터마이닝[DataMining]'):
         DataToProcess(location).DataMining(name)
 ###############################################################################
+if args.dAnalysis:
     for dataname in tqdm(FindDataFiles(location['project'], 'zip').keys(), desc='데이터분석[DataAnalysis]'):
         Criterion = DataAnalysis(location['conf']).BaseLine(dataname)
         Object = DataUnpack(location['project']).UnpackExtract(dataname)
         Proto = DataAnalysis(location['conf']).BaseReport(dataname)
         dCairos(Criterion, Object, Proto).dWorkerRun(location['output']+'/'+ dataname)
+###############################################################################
+if args.aControl:
+    for name in tqdm(os.listdir(location['input']), desc='데이터마이닝[DataMining]'):
+        DataToProcess(location).DataMining(name)
+    for dataname in tqdm(FindDataFiles(location['project'], 'zip').keys(), desc='데이터분석[DataAnalysis]'):
+        Criterion = DataAnalysis(location['conf']).BaseLine(dataname)
+        Object = DataUnpack(location['project']).UnpackExtract(dataname)
+        Proto = DataAnalysis(location['conf']).BaseReport(dataname)
+        dCairos(Criterion, Object, Proto).dWorkerRun(location['output']+'/' + dataname)
